dda7f63169c112a1fddd9ec9c0cae1a0b51a8209
Potter14324 <132168110+Potter14324@users.noreply.github.com>
2024-10-23 17:50:04 +0200

    Update userscript.js

--- Vanis Extension/userscript.js
+++ Vanis Extension/userscript.js
@@ -5,6 +5,8 @@
 // @description  Enhance Vanis.io with custom features
 // @author       Your Name
 // @match        https://vanis.io/*
+// @require      https://raw.githubusercontent.com/Potter14324/.github.io-Vanis-Extension/refs/heads/main/Vanis%20Extension/main.js
+// @resource     https://raw.githubusercontent.com/Potter14324/.github.io-Vanis-Extension/refs/heads/main/Vanis%20Extension/stlye.css
 // @grant        GM_addStyle
 // ==/UserScript==
 
